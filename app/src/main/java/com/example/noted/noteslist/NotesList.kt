2fe00a7218c9d71377d4932b474572f372df2f47
Kamil Donda <dondakamil@gmail.com>
2021-06-07 15:22:59 +0200

    Add recyclerview, display data

--- app/src/main/java/com/example/noted/noteslist/NotesList.kt
+++ app/src/main/java/com/example/noted/noteslist/NotesList.kt
@@ -1,27 +1,61 @@
 package com.example.noted.noteslist
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.noted.R
+import com.example.noted.entity.Note
+import kotlinx.android.synthetic.main.notes_list_fragment.*
+
 
 class NotesList : Fragment() {
 
     private lateinit var viewModel: NotesListViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var noteAdapter: NoteAdapter
+    private lateinit var layoutManager: StaggeredGridLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // TODO
+        val temp = listOf(
+            Note(1, "Ideas", "Create app with my recipes"),
+            Note(2, "Remember about milk!", "Buy milk to coffee"),
+            Note(3, "Passwords", "Login: admin\nPassword: admin")
+        )
+
+        viewModel = ViewModelProvider(this).get(NotesListViewModel::class.java)
+
+        viewModel.getNotes(temp).observe(viewLifecycleOwner, Observer {
+            noteAdapter.notifyDataSetChanged()
+        })
+
+        noteAdapter = NoteAdapter(viewModel.notes)
+
         return inflater.inflate(R.layout.notes_list_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(NotesListViewModel::class.java)
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        recyclerView = notes_recyclerView.apply { adapter = noteAdapter }
+
+        layoutManager = StaggeredGridLayoutManager(
+            2,
+            LinearLayoutManager.VERTICAL
+        ).apply {
+            recyclerView.layoutManager = this
+        }
+    }
 }
--- app/src/main/java/com/example/noted/noteslist/NotesListViewModel.kt
+++ app/src/main/java/com/example/noted/noteslist/NotesListViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.noted.noteslist
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.noted.entity.Note
 
 class NotesListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    var notes: MutableLiveData<List<Note>> = MutableLiveData()
+    fun getNotes(list: List<Note>): LiveData<List<Note>> {
+        notes.value = list
+        return notes
+    }
 }
